dc562ac79527f0a7e86e2145a968bee608ccfdf8
epassi <1994876+epassi@users.noreply.github.com>
2020-06-14 08:52:56 -0700

    Restore flask_cors

--- service.py
+++ service.py
@@ -1,17 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-# from sklearn.externals import joblib
+from flask_cors import CORS
 import joblib
 
 app = Flask(__name__)
-# app.config['CORS_HEADERS'] = 'Content-Type'
-
 CORS(app)
-
-
-
-# model = joblib.load(open('median-house-value.pkl', 'rb'))
 model = joblib.load('median-house-value.joblib')
 
 @app.route('/', methods=['GET'])
@@ -24,13 +17,6 @@ def results():
     prediction = model.predict([np.array(list(data.values()))])
     output = prediction[0]
     return jsonify(output)
-
-# @app.after_request
-# def after_request(response):
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-#     return response
 
 if __name__ == "__main__":
     app.run(debug=True)
